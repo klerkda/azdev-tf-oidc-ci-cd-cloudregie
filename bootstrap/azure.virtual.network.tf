ddb26c15f1de0d136c1be7321b0348cbbe16afb4
David de Klerk <david.deklerk@prorail.nl>
2025-05-06 15:47:20 +0200

    fixed vnet

--- bootstrap/azure.virtual.network.tf
+++ bootstrap/azure.virtual.network.tf
@@ -1,16 +1,16 @@
 data "azurerm_virtual_network" "virtual_network" {
-  name                = "PR-CLRP-VNET01"
-  resource_group_name = "PR-CLRP-VirtualNetworks"
+  name                = "PR-Lighthouse-VNET01"
+  resource_group_name = "PR-Lighthouse-VirtualNetworks"
 }
 
 data "azurerm_subnet" "agents" {
   name                 = "agents"
-  resource_group_name  = "PR-CLRP-VirtualNetworks"
-  virtual_network_name = "PR-CLRP-VNET01"
+  resource_group_name  = "PR-Lighthouse-VirtualNetworks"
+  virtual_network_name = "PR-Lighthouse-VNET01"
 }
 
 data "azurerm_subnet" "private_endpoints" {
   name                 = "private_endpoints"
-  resource_group_name  = "PR-CLRP-VirtualNetworks"
-  virtual_network_name = "PR-CLRP-VNET01"
+  resource_group_name  = "PR-Lighthouse-VirtualNetworks"
+  virtual_network_name = "PR-Lighthouse-VNET01"
 }
